6e1b8fa379f5cd722933ea1f9e7b9b96bab8947a
Lorenzo Bragaglia <lbragaglia@users.noreply.github.com>
2023-11-02 20:29:16 +0100

    Use feature management api

--- src/WeatherWebApi/Program.cs
+++ src/WeatherWebApi/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.FeatureManagement;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddFeatureManagement(/*builder.Configuration.GetSection("FeatureFlags")*/);
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -13,9 +16,7 @@ app.UseHttpsRedirection();
 
 var weatherService = new WeatherService();
 
-var forecastEnabled = builder.Configuration.GetValue<bool>("FeatureFlags:WeatherForecast");
-
-app.MapGet("/weatherforecast", () => forecastEnabled
+app.MapGet("/weatherforecast", async (IFeatureManager manager) => await manager.IsEnabledAsync("WeatherForecast")
         ? Results.Ok(weatherService.Get())
         : Results.NotFound())
     .WithName("GetWeatherForecast")
