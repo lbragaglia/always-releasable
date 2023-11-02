db636fc709b864c943d86da60a322a0c89a70c9c
Lorenzo Bragaglia <lbragaglia@users.noreply.github.com>
2023-11-02 21:58:15 +0100

    Make custom feature filter contextual

--- src/WeatherWebApi/MyCustomFilter.cs
+++ src/WeatherWebApi/MyCustomFilter.cs
@@ -1,17 +1,22 @@
 using Microsoft.FeatureManagement;
 
 [FilterAlias(nameof(MyCustomFilter))]
-public class MyCustomFilter : IFeatureFilter
+public class MyCustomFilter : IContextualFeatureFilter<MyCustomFilterContext>
 {
-    public Task<bool> EvaluateAsync(FeatureFilterEvaluationContext context)
+    public Task<bool> EvaluateAsync(FeatureFilterEvaluationContext context, MyCustomFilterContext appContext)
     {
         var settings = context.Parameters.Get<MyCustomFilterSettings>()
                        ?? throw new ArgumentNullException(nameof(MyCustomFilterSettings));
-        return Task.FromResult(settings.LuckyNumber == 47);
+        return Task.FromResult(settings.LuckyNumber == appContext.InputNumber);
     }
 }
 
 public class MyCustomFilterSettings
 {
     public int LuckyNumber { get; set; }
+}
+
+public class MyCustomFilterContext
+{
+    public int InputNumber { get; set; }
 }
--- src/WeatherWebApi/Program.cs
+++ src/WeatherWebApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.FeatureManagement;
 using Microsoft.FeatureManagement.FeatureFilters;
 
@@ -21,9 +22,12 @@ app.UseHttpsRedirection();
 
 var weatherService = new WeatherService();
 
-app.MapGet("/weatherforecast", async (IFeatureManager manager) => await manager.IsEnabledAsync("WeatherForecast")
-        ? Results.Ok(weatherService.Get())
-        : Results.NotFound())
+app.MapGet("/weatherforecast",
+        async (IFeatureManager manager, [FromHeader(Name = "X-Lucky-Number")] int? inputNumber) =>
+            await manager.IsEnabledAsync("WeatherForecast",
+                new MyCustomFilterContext { InputNumber = inputNumber ?? 0 })
+                ? Results.Ok(weatherService.Get())
+                : Results.NotFound())
     .WithName("GetWeatherForecast")
     .WithOpenApi();
 
